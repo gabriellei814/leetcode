8934ae29933dea709532595aa85b6c31024bd6f1
gabriellei814 <yihuilei814@gmail.com>
2017-01-03 23:59:52 -0800

    Modified solution276 class name

---
+++ Java/Solution276.java
@@ -0,0 +1,15 @@
+public class Solution276 {
+    public int numWays(int n, int k) {
+        if(n == 0) return 0;
+        if(n == 1) return k;
+
+        int diff = k * (k - 1);
+        int same = k;
+        for(int i = 2; i < n; i++) {
+            int temp = diff;
+            diff = (same + diff) * (k - 1);
+            same = temp;
+        }
+        return same + diff;
+    }
+}
